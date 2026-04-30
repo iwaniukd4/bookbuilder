c61c644f62cbeaa353f1e556eb033d408bb5d723
iwaniukd4 <iwaniukd4@gmail.com>
2026-04-30 12:26:50 +0200

    Created internal and exernal (with Stripe) buy book API infrastructure.

--- server/api/customer.js
+++ server/api/customer.js
@@ -11,4 +11,35 @@ router.use((req, res, next) => {
   next();
 });
 
+router.post('/stripe/fetch-checkout-session', async (req, res) => {
+  try {
+    const { bookId, redirectUrl } = req.body;
+
+    const book = await Book.findById(bookId).select(['slug']).setOptions({ lean: true });
+
+    if (!book) {
+      throw new Error('Book not found');
+    }
+
+    const isPurchased =
+      (await Purchase.find({ userId: req.user._id, bookId: book._id }).countDocuments()) > 0;
+    if (isPurchased) {
+      throw new Error('Already bought this book');
+    }
+
+    const session = await createSession({
+      userId: req.user._id.toString(),
+      userEmail: req.user.email,
+      bookId,
+      bookSlug: book.slug,
+      redirectUrl,
+    });
+
+    res.json({ sessionId: session.id });
+  } catch (err) {
+    console.error(err);
+    res.json({ error: err.message || err.toString() });
+  }
+});
+
 module.exports = router;
